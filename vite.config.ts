c3d5a1dbf4a6989687e5dcd662e382aa83c6b234
Kien Le <71136908+kien096@users.noreply.github.com>
2025-12-22 13:20:50 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/", // Dùng "/" nếu deploy ở root domain
+    base: "/Landing-page-LMS/",
     server: {
       port: 3000,
       host: "0.0.0.0",
